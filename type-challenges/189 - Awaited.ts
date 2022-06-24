ed03d6a9ff42384f3b25e8f0c6fc35712a86569d
wsszh01111 <wsszh01111@163.com>
2022-06-24 10:50:22 +0800

    challenges: 189 - Awaited

--- type-challenges/189 - Awaited.ts
+++ type-challenges/189 - Awaited.ts
@@ -1,4 +1,4 @@
 /**
  * https://github.com/type-challenges/type-challenges/blob/main/questions/00189-easy-awaited/README.md
  */
-type MyAwaited<T> = T extends Promise<infer K> ? MyAwaited<K> : K
+type MyAwaited<T> = T extends Promise<infer K> ? MyAwaited<K> : T
